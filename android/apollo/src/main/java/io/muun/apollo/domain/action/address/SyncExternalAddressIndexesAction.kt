442f22ba04b8537380e7e95a7346a85e92358b25
TerraStake.org <emiliano.solazzi@terrastake.org>
2025-03-27 19:04:04 -0400

    Update SyncExternalAddressIndexesAction.kt

--- android/apollo/src/main/java/io/muun/apollo/domain/action/address/SyncExternalAddressIndexesAction.kt
+++ android/apollo/src/main/java/io/muun/apollo/domain/action/address/SyncExternalAddressIndexesAction.kt
@@ -1,41 +1,64 @@
 package io.muun.apollo.domain.action.address
 
-import io.muun.apollo.data.net.HoustonClient
-import io.muun.apollo.data.preferences.KeysRepository
-import io.muun.apollo.domain.action.base.BaseAsyncAction0
-import io.muun.apollo.domain.utils.toVoid
-import io.muun.common.api.ExternalAddressesRecord
-import io.muun.common.utils.MathUtils.maxWithNulls
+import io.muun.apollo.domain.action.base.BaseAsyncAction1
+import io.muun.apollo.domain.action.incoming_swap.GenerateInvoiceAction
+import io.muun.apollo.domain.libwallet.DecodedBitcoinUri
+import io.muun.apollo.domain.libwallet.Invoice
+import io.muun.apollo.domain.model.BitcoinAmount
+import io.muun.apollo.domain.model.MuunAddressGroup
+import org.bitcoinj.core.NetworkParameters
 import rx.Observable
+import rx.schedulers.Schedulers
+import timber.log.Timber
 import javax.inject.Inject
 
+class GenerateBip21UriAction @Inject constructor(
+    private val networkParameters: NetworkParameters,
+    private val generateInvoice: GenerateInvoiceAction,
+    private val createAddress: CreateAddressAction
+) : BaseAsyncAction1<BitcoinAmount?, DecodedBitcoinUri>() {
 
-open class SyncExternalAddressIndexesAction @Inject constructor(
-    val houstonClient: HoustonClient,
-    val keysRepository: KeysRepository
-): BaseAsyncAction0<Void>() {
-
-    /**
-     * Sync the external address indexes with Houston.
-     */
-    override fun action(): Observable<Void> =
-        Observable.defer {
-            updateOrFetchRecord().map(this::storeRecord).toVoid()
-        }
-
-    private fun updateOrFetchRecord() =
-        keysRepository.maxUsedExternalAddressIndex.let {
-            if (it != null)
-                houstonClient.updateExternalAddressesRecord(it)
-            else
-                houstonClient.fetchExternalAddressesRecord()
-        }
-
-    private fun storeRecord(record: ExternalAddressesRecord) {
-        keysRepository.maxUsedExternalAddressIndex =
-            maxWithNulls(record.maxUsedIndex, keysRepository.maxUsedExternalAddressIndex)
-
-        keysRepository.maxWatchingExternalAddressIndex =
-            maxWithNulls(record.maxWatchingIndex, keysRepository.maxWatchingExternalAddressIndex)
+    companion object {
+        private const val TAG = "GenerateBip21Uri"
+        private const val INVOICE_DECODE_ERROR = "Invoice decode failed"
     }
+
+    override fun action(amount: BitcoinAmount?): Observable<DecodedBitcoinUri> {
+        return Observable.zip(
+            getAddressGroup(),
+            getInvoice(amount),
+            ::createDecodedUri
+        )
+        .onErrorResumeNext { error ->
+            Timber.tag(TAG).e(error, "BIP21 URI generation failed")
+            Observable.error(error)
+        }
+        .subscribeOn(Schedulers.io())
+    }
+
+    private fun getAddressGroup(): Observable<MuunAddressGroup> {
+        return createAddress.action()
+            .doOnNext { Timber.tag(TAG).d("Address group generated") }
+            .map(MuunAddressGroup::toAddressGroup)
+    }
+
+    private fun getInvoice(amount: BitcoinAmount?): Observable<String> {
+        return generateInvoice.action(amount?.inSatoshis)
+            .doOnNext { Timber.tag(TAG).d("Invoice generated for amount: ${amount?.inSatoshis} sat") }
+    }
+
+    private fun createDecodedUri(addressGroup: MuunAddressGroup, invoice: String): DecodedBitcoinUri {
+        return try {
+            val decodedInvoice = Invoice.decodeInvoice(networkParameters, invoice)
+                ?: throw IllegalArgumentException("Null invoice returned from decoder")
+            
+            Timber.tag(TAG).v("Invoice successfully decoded")
+            DecodedBitcoinUri(addressGroup, decodedInvoice, amount)
+        } catch (e: Exception) {
+            Timber.tag(TAG).e(e, INVOICE_DECODE_ERROR)
+            throw InvoiceDecodeException(INVOICE_DECODE_ERROR, e)
+        }
+    }
+
+    class InvoiceDecodeException(message: String, cause: Throwable) : Exception(message, cause)
 }
