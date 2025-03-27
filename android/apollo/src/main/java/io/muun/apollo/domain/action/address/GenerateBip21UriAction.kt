f4e4484515efb1e36e2bcc11166d527ee5fe6308
TerraStake.org <emiliano.solazzi@terrastake.org>
2025-03-27 19:02:50 -0400

    Update GenerateBip21UriAction.kt

--- android/apollo/src/main/java/io/muun/apollo/domain/action/address/GenerateBip21UriAction.kt
+++ android/apollo/src/main/java/io/muun/apollo/domain/action/address/GenerateBip21UriAction.kt
@@ -8,24 +8,40 @@ import io.muun.apollo.domain.model.BitcoinAmount
 import io.muun.apollo.domain.model.MuunAddressGroup
 import org.bitcoinj.core.NetworkParameters
 import rx.Observable
+import timber.log.Timber
 import javax.inject.Inject
 
 class GenerateBip21UriAction @Inject constructor(
     private val networkParameters: NetworkParameters,
     private val generateInvoice: GenerateInvoiceAction,
-    private val createAddress: CreateAddressAction,
+    private val createAddress: CreateAddressAction
 ) : BaseAsyncAction1<BitcoinAmount?, DecodedBitcoinUri>() {
+
+    companion object {
+        private const val TAG = "GenerateBip21Uri"
+    }
 
     override fun action(amount: BitcoinAmount?): Observable<DecodedBitcoinUri> {
         return Observable.zip(
-            createAddress.action().map(MuunAddressGroup::toAddressGroup),
+            createAddress.action()
+                .doOnNext { Timber.tag(TAG).d("Generated new address group") }
+                .map(MuunAddressGroup::toAddressGroup),
+            
             generateInvoice.action(amount?.inSatoshis)
-        ) { addressGroup, invoice ->
-            DecodedBitcoinUri(
-                addressGroup,
-                Invoice.decodeInvoice(networkParameters, invoice),
-                amount
-            )
+                .doOnNext { Timber.tag(TAG).d("Generated invoice for amount: $amount") },
+            
+            { addressGroup, invoice ->
+                try {
+                    val decodedInvoice = Invoice.decodeInvoice(networkParameters, invoice)
+                    Timber.tag(TAG).v("Successfully decoded invoice")
+                    DecodedBitcoinUri(addressGroup, decodedInvoice, amount)
+                } catch (e: Exception) {
+                    Timber.tag(TAG).e(e, "Failed to decode invoice")
+                    throw e
+                }
+            }
+        ).doOnError { error ->
+            Timber.tag(TAG).e(error, "Failed to generate BIP21 URI")
         }
     }
 }
