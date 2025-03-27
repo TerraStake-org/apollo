151dbb04711bf7cb6eed4a1295bc6bb6fdbe6a2a
TerraStake.org <emiliano.solazzi@terrastake.org>
2025-03-27 02:58:25 -0400

    Update FirebaseManager.kt

--- android/apollo/src/main/java/io/muun/apollo/data/async/gcm/FirebaseManager.kt
+++ android/apollo/src/main/java/io/muun/apollo/data/async/gcm/FirebaseManager.kt
@@ -1,75 +1,81 @@
 package io.muun.apollo.data.async.gcm
 
-import android.os.AsyncTask
-import com.google.android.gms.tasks.Task
 import com.google.firebase.messaging.FirebaseMessaging
 import io.muun.apollo.domain.action.fcm.UpdateFcmTokenAction
 import io.muun.apollo.domain.errors.MuunError
 import io.muun.apollo.domain.errors.fcm.FcmTokenCanceledError
 import io.muun.apollo.domain.errors.fcm.FcmTokenError
-import rx.Observable
-import rx.subjects.BehaviorSubject
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.subjects.BehaviorSubject
 import timber.log.Timber
 import java.io.IOException
+import java.util.concurrent.Executor
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class FirebaseManager @Inject constructor(private val updateFcmTokenAction: UpdateFcmTokenAction) {
+class FirebaseManager @Inject constructor(
+    private val updateFcmTokenAction: UpdateFcmTokenAction,
+    private val ioExecutor: Executor
+) {
 
-    private val subject = BehaviorSubject.create<String>()
+    private val tokenSubject = BehaviorSubject.create<String>()
 
     /**
-     * Manually fetch FCM token. Useful for force-fetching when hasn't arrive via
-     * {@link GcmMessageListenerService#onNewToken}.
+     * Manually fetch FCM token. Useful for force-fetching when hasn't arrived via
+     * FirebaseMessagingService#onNewToken.
+     * 
+     * @return Observable that emits the FCM token or an error
      */
     fun fetchFcmToken(): Observable<String> {
-
-        FirebaseMessaging.getInstance().token
-            .addOnCompleteListener { task ->
-
-                if (!task.isSuccessful) {
-                    val error = getError(task)
+        FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
+            when {
+                task.isCanceled -> {
+                    val error = FcmTokenCanceledError()
                     Timber.e(error)
-                    subject.onError(error)
-                    return@addOnCompleteListener
+                    tokenSubject.onError(error)
                 }
-
-                val token = checkNotNull(task.result)
-
-                Timber.i("GetInstanceId SUCCESS: $token")
-
-                updateFcmTokenAction.run(token)
-                subject.onNext(token)
+                !task.isSuccessful -> {
+                    val error = FcmTokenError(
+                        task.exception ?: RuntimeException("Unknown FCM token error")
+                    )
+                    Timber.e(error)
+                    tokenSubject.onError(error)
+                }
+                else -> {
+                    val token = requireNotNull(task.result) { "FCM token was null despite successful task" }
+                    Timber.i("FCM token fetch SUCCESS: $token")
+                    updateFcmToken(token)
+                    tokenSubject.onNext(token)
+                }
             }
+        }
 
-        return subject
+        return tokenSubject.hide() // Prevent subject errors from terminating the observable
     }
 
-    private fun getError(task: Task<String>): MuunError {
-        return if (task.isCanceled) {
-            FcmTokenCanceledError()
-
-        } else {
-            FcmTokenError(task.exception ?: RuntimeException("Null underlying error!"))
+    /**
+     * Delete FCM Token, forcing a new token generation which should trigger
+     * FirebaseMessagingService#onNewToken.
+     */
+    fun resetFcmToken() {
+        ioExecutor.execute {
+            try {
+                FirebaseMessaging.getInstance().deleteToken()
+                Timber.i("FCM token successfully deleted")
+            } catch (e: IOException) {
+                Timber.e(FcmTokenError(e))
+            } catch (e: Exception) {
+                Timber.e(FcmTokenError(e))
+            }
         }
     }
 
-    /**
-     * Delete FCM Token, forcing a new token generation, which should arrive via/trigger
-     * {@link GcmMessageListenerService#onNewToken}.
-     */
-    private fun resetFcmToken() {
-
-        // This needs to happen outside the main thread (see code for deleteInstanceId())
-        AsyncTask.execute {
-            try {
-                FirebaseMessaging.getInstance().deleteToken()
-                Timber.i("InstanceID DELETED")
-
-            } catch (e: IOException) {
-                Timber.e(FcmTokenError(e))
-            }
+    private fun updateFcmToken(token: String) {
+        try {
+            updateFcmTokenAction.run(token)
+        } catch (e: Exception) {
+            Timber.e(FcmTokenError(e))
         }
     }
 }
